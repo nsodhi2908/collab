65f8aeecf1baba92696b57650464d3e6aa67fb2c
Kohaye <korey.hayes@flemingcollege.ca>
2021-04-06 18:35:25 -0400

    I'm done for the day

--- download.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-
-url = 'https://kf.kobotoolbox.org/api/v2/assets/ayYd637KWJhM2eSfAKcjFN/data/?format=json'
-r = requests.get(url, allow_redirects=True)
-
-open('Kobo/geoJSON.geojson', 'wb').write(r.content)
