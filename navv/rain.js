a89b6c939c89c0badb336521a2ca1708de94fc41
nsodhi2908 <navjotkaur.sodhi@flemingcollege.ca>
2021-04-07 08:45:08 -0400

    k

---
+++ navv/rain.js
@@ -0,0 +1,11 @@
+ // Change default options
+ L.control.rainviwer({ 
+    position: 'bottomleft',
+    nextButtonText: '>',
+    playStopButtonText: 'Play/Stop',
+    prevButtonText: '<',
+    positionSliderLabelText: "Hour:",
+    opacitySliderLabelText: "Opacity:",
+    animationInterval: 500,
+    opacity: 0.5
+}).addTo(map);
