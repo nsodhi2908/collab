4974055a6671d58c42ca0566c02cb96541d53cbb
Kohaye <korey.hayes@flemingcollege.ca>
2021-05-18 19:11:13 -0400

    requests back to prev

--- validation/gdalTest.py
+++ validation/gdalTest.py
@@ -1,5 +1,16 @@
-import sys
-try:
-    from osgeo import ogr, osr, gdal
-except:
-    sys.exit('ERROR: cannot find GDAL/OGR modules')
+from osgeo import ogr
+# geojson = open(r"Kobo\geoJSON.geojson", "r")
+geojson = """{"type":"Point","coordinates":[108420.33,753808.59]}"""
+point = ogr.CreateGeometryFromJson(geojson)
+print ("%d,%d" % (point.GetX(), point.GetY()))
+
+# import urllib, geojson, gdal, subprocess
+# url= ' http://ig3is.grid.unep.ch/istsos/wa/istsos/services/ghg/procedures/operations/geojson?epsg=4326'
+# response = urllib.urlopen(url)
+# data = geojson.loads(response.read())
+
+# with open('data.geojson', 'w') as f:
+#     geojson.dump(data, f)
+
+# args = ['ogr2ogr', '-f', 'ESRI Shapefile', 'destination_data.shp', 'data.geojson']
+# subprocess.Popen(args)
